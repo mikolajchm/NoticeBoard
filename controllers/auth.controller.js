dd3548a51dd37c7b56ba65cc5d442460cae4c4e2
mikolajchm <chmielewski.5454@gmail.com>
2024-08-29 15:06:07 +0200

    Add login-option

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,7 +9,6 @@ exports.register = async (req, res) => {
         if(userWithLogin) {
            return  res.status(409).send({ message: 'User with this login already exists' });
         }
-
         const user = await User.create({ login, password: await bcrypt.hash(password, 10), phone});
         res.status(201).send({ message: 'User created' + user.login });
     } else {
@@ -22,9 +21,31 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-
-};
+    try { 
+        const { login, password } = req.body;
+        if (login && typeof login === 'string' && password && typeof password === 'string') {
+        const user = await User.findOne({ login });
+            if(!user) {
+                res.status(400).send({ message: 'Login or password are ircorrect' });
+            } else {
+                if(bcrypt.compareSync(password, user.password)) {
+                    res.status(200).send({ message: 'Login succesful' });
+                } else {
+                    res.status(400).send({ message: 'Login or password are ircorrect' });
+                    }
+                }
+        } else {
+            res.status(400).send({ message: 'Bad request' });
+            }
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+}; 
 
 exports.user = async (req, res) => {
+try { 
 
+} catch (err) {
+    res.status(500).send({ message: err.message });
+}
 };
