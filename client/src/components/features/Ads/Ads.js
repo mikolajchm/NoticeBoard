82046ccf6a156fc3af04388a659176d9e72b1c6b
mikolajchm <chmielewski.5454@gmail.com>
2024-09-25 16:09:10 +0200

    Ad single ad page

--- client/src/components/features/Ads/Ads.js
+++ client/src/components/features/Ads/Ads.js
@@ -1,14 +1,36 @@
+import { IMGS_URL } from '../../../config';
+import { getAllAds } from '../../../redux/adsRedux'
 import Container from 'react-bootstrap/Container';
+import { useSelector } from 'react-redux';
 import Row from 'react-bootstrap/Row';
-
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
 
 const Ads = () => {
+  const ads = useSelector(getAllAds);
+
   return (
-    <Container>
-      <Row>
-     <h1>ads</h1>
-      </Row>
-    </Container>
+    <>
+    <Row xs={1} md={2} xl={3} className="mb-5">
+      {ads.map((ad) => (
+        <Col key={ad._id} className="mb-4 d-flex justify-content-center">  
+            <Card style={{  width: '100%' , padding: '2px' }}>   
+            <Image style={{ height: '200px', objectFit: 'cover' }} src={IMGS_URL + ad.photo} rounded />
+            <Card.Body>
+              <Card.Title>{ad.title}</Card.Title>
+              <Card.Text className="mb-3 text-muted">{ad.location}</Card.Text>
+              <Link to={`/ad/${ad._id}`}>
+                <Button variant="outline-primary" size="lg">Read more...</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  </>
   );
 }
 
--- client/src/components/pages/Ad/Ad.js
+++ client/src/components/pages/Ad/Ad.js
@@ -1,15 +1,91 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { IMGS_URL } from '../../../config';
+import { useSelector } from 'react-redux';
+import { getAdById } from '../../../redux/adsRedux';
+import { getLoggedUser } from '../../../redux/userRedux';
+import { useParams } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/esm/Image';
+
 
 const Ad = () => {
+    const { id } = useParams();
+    const adData = useSelector((state) => getAdById(state, id));
+    const loggedUser = useSelector(getLoggedUser);
+
     return (
         <Form>
-            <Button  variant="outline-success" type="submit"> 
-                Edit
-            </Button> 
-            <Button  className="ms-3" variant="danger" type="submit"> 
-                Remove 
-            </Button> 
+            <Form.Group className="mb-2">
+                <Card style={{ padding: '5px'}}>
+                    <Card.Header><cite>Information :</cite></Card.Header>
+                    <Card.Body >
+                        <blockquote className="blockquote mb-3">
+                            <Col xs={6} md={4} xl={8} className="mb-3">
+                                <Image src={IMGS_URL + adData.photo} fluid />
+                            </Col>
+                            <p>
+                                Title : 
+                            </p> 
+                            <footer className="blockquote-footer" style={{color: 'black'}}>
+                                {adData.title}  
+                            </footer>
+                            <p>
+                                Description : 
+                            </p>
+                            <footer className="blockquote-footer" style={{color: 'black'}}>
+                                {adData.content}  
+                            </footer>
+                            <p>
+                                PublishDate : 
+                            </p>
+                            <footer className="blockquote-footer" style={{color: 'black'}}>
+                                {adData.publishDate}
+                            </footer>
+                            <p>
+                                Price : 
+                            </p>
+                            <footer className="blockquote-footer" style={{color: 'black'}}>
+                                {adData.price}
+                            </footer>
+                            <p>
+                                Location : 
+                            </p>
+                            <footer className="blockquote-footer" style={{color: 'black'}}>
+                                {adData.location}
+                            </footer>
+                            <p>
+                                Sellerinfo : 
+                            </p>
+                            <p className="blockquote-footer" style={{color: 'black'}}>
+                                Login : {adData.login}
+                            </p>
+                            <p  className="blockquote-footer" style={{color: 'black'}}>
+                                Avatar : {''}
+                                <Image src={IMGS_URL + adData.photo}  style={{width: '200px'}}/>
+                            </p>
+                            <p className="blockquote-footer" style={{color: 'black'}}>
+                                Phone : {adData.phone}
+                            </p>
+                            {loggedUser && (
+                                <div>
+                                    <Link to={`/ad/edit/${id}`}>
+                                        <Button  variant="outline-success" type="submitedit"> 
+                                            Edit
+                                        </Button> 
+                                    </Link>
+                                    <Button  className="ms-3" variant="danger" type="submitremove"> 
+                                        Remove 
+                                    </Button>
+                                </div>
+                            )}
+                        </blockquote>
+                    </Card.Body>
+                </Card>
+            </Form.Group>
         </Form>
     )
 }
